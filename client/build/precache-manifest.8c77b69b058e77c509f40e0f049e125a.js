2314879e8b552800ee5e61109687fa82eee2946f
Maggie MacVey <maggiemacvey7@gmail.com>
2019-10-17 21:32:06 -0400

    meow

---
+++ client/build/precache-manifest.8c77b69b058e77c509f40e0f049e125a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d6b8d5a0ca39adcd4d5e",
+    "url": "/static/css/main.a93d2971.chunk.css"
+  },
+  {
+    "revision": "d6b8d5a0ca39adcd4d5e",
+    "url": "/static/js/main.bbea4ced.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e0ecd5b1aef65dac07ed",
+    "url": "/static/js/2.3333d970.chunk.js"
+  },
+  {
+    "revision": "20b9bbd085cd7755e27d9b82b8ecebc3",
+    "url": "/index.html"
+  }
+];
